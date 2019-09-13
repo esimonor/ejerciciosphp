df6ecfb69b48cf5268b14a16f61a12319126a3ca
esimonor <esimonor18dw@ikzubirimanteo.com>
2019-09-13 09:52:19 +0200

    subo el ejercicio 1

---
+++ ej_1.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Ejercicio 1</title>
+    </head>
+    <body>
+
+        <?php
+            $puertas = 4;
+            for($pisos=1; $pisos <=5; $pisos++){
+            echo "Piso ",$pisos," puertas ",$puertas,"<br>";
+            }
+        ?>
+
+    </body>
+</html>
