d20c838cbd495a3594e56a5e60d0d66a618f353e
esimonor <44833818+esimonor@users.noreply.github.com>
2019-09-24 09:53:08 +0200

    Ejercicio 3 resuelto

--- ej_3.php
+++ ej_3.php
@@ -1,21 +1,24 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Ejercicio 1</title>
+        <title>Ejercicio 3</title>
     </head>
     <body>
 
         <?php
         $edad=rand(0,100);
-        $min=$edad-10;
-        $max=$edad+10;
-        if($min<0){
-            $min=0;
+        if($edad > 0 && $edad <= 20){
+            echo("La edad está entre 0 y 20 años <br>");
+        } else if ($edad >20 && $edad <=40){
+            echo("La edad está entre 20 y 40 años <br>");
+        } else if ($edad >40 && $edad <=60){
+            echo("La edad está entre 40 y 60 años <br>");
+        } else if ($edad >60 && $edad <=80){
+            echo("La edad está entre 60 y 80 años <br>");
+        } else if ($edad >80 && $edad <=100){
+            echo("La edad está entre 80 y 100 años <br>");
         }
-        if($max>100){
-            $max=100;
-        }
-        echo "El rango de edad de ",$edad," esta entre ",$min, " y ", $max;
+        echo "La edad exacta es ". $edad;
         ?>
 
     </body>
