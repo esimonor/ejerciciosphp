a13a60f8cc7df512534d07a15126a64d190baa13
esimonor <44833818+esimonor@users.noreply.github.com>
2019-09-30 17:11:45 +0200

    Bloque2 Ejercicio7 completado

---
+++ Bloque2_Ej7.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+	<title>Ejericio 7</title>
+</head>
+<body>
+	<?php
+	$resultado=0;
+
+	function berreketa($num1,$num2){
+		$resultado=pow($num1, $num2);
+
+		return $resultado;
+
+	}
+	echo berreketa(3,4);
+
+	?>
+
+</body>
+</html>
