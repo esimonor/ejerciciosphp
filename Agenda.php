fdb17524fe69a7a11e2ca0a15f7e5608dec73c74
esimonor <44833818+esimonor@users.noreply.github.com>
2019-10-10 09:08:26 +0200

    Ejercicio Agenda

--- Agenda.php
+++ Agenda.php
@@ -15,15 +15,6 @@
         
         if($nombre==""){
             echo "<p>Por favor introduzca un nombre valido";
-            /*$file = fopen("archivo.txt", "r");
-
-            while(!feof($file)) {
-            
-            echo fgets($file). "<br />";
-            
-            }
-            
-            fclose($file);*/
         }
         else{
             echo "<p>Nombre contacto:$nombre</p><p>Correo de $nombre:$correo</p>";
@@ -31,7 +22,7 @@
         ?>
 
         <form action="#" method="post">
-        <p>Nombre <input type="text" name="nombre" placeholder="Nombre del contacto" pattern="[^'\x22]+"></p>
+        <p>Nombre <input type="text" name="nombre" placeholder="Nombre del contacto" pattern="[A-Za-z0-9_]{1,15}"></p>
         <p>Correo <input type="text" name="correo" placeholder="Emilio del contacto"></p>
         <input type="submit" value="Añadir contacto">
         </form>
